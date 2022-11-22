c5514ec7dddb71ec7846870d178d02caca8645af
Алексей Горемыкин <goremykin.jr@outlook.com>
2022-11-22 15:00:16 +0500

    add switcher

---
+++ switcher.py
@@ -0,0 +1,11 @@
+from main import *
+
+choise = input('Что вывести?')
+
+report = Report()
+
+match choise:
+    case 'Вакансии':
+        report.generate_excel(data[:5], data[5:])
+    case 'Статистика':
+        report.generate_image(data)
